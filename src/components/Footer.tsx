cdcf547a4fab8bad3fcf84a79345a8172d3c5285
Michael <michaelpetterson06.com.br>
2023-06-11 14:13:58 -0300

    Criando header e footer

---
+++ src/components/Footer.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+// import styles from "./Footer.module.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>
+        <span>React + TS Todo</span> @ 2023
+      </p>
+      <p>
+        Michael Petterson
+      </p>
+    </footer>
+  );
+};
+
+export default Footer;
