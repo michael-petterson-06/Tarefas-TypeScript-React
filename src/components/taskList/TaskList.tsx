2d64b827174cced9f6444abb1a57a50479afe623
Michael <michaelpetterson06.com.br>
2023-06-11 20:46:29 -0300

    Finalizando projeto

--- src/components/taskList/TaskList.tsx
+++ src/components/taskList/TaskList.tsx
@@ -1,39 +1,39 @@
 import React from "react";
 
-//interfaces
+// interfaces
 import { ITask } from "../../interfaces/Task";
 
-//Css
 import styles from "./TaskList.module.css";
 
 interface Props {
   taskList: ITask[];
-  handleDelete(id: number): void;
+  handleDelete(title: string): void;
+  handleEdit(task: ITask): void;
 }
 
-const TaskList = ({ taskList, handleDelete,}: Props) => {
+const TaskList = ({ taskList, handleDelete, handleEdit }: Props) => {
   return (
     <>
-    {taskList.length > 0 ? (
-      taskList.map((task, index) => (
-        <div key={index} className={styles.task}>
-          <div className={styles.details}>
-            <h4>{task.title}</h4>
-            <p>Dificuldade: {task.difficulty}</p>
+      {taskList.length > 0 ? (
+        taskList.map((task, index) => (
+          <div key={index} className={styles.task}>
+            <div className={styles.details}>
+              <h4>{task.title}</h4>
+              <p>Dificuldade: {task.difficulty}</p>
+            </div>
+            <div className={styles.actions}>
+              <i className="bi bi-pencil" onClick={() => handleEdit(task)}></i>
+              <i
+                className="bi bi-trash"
+                onClick={() => handleDelete(task.title)}
+              ></i>
+            </div>
           </div>
-          <div className={styles.actions}>
-            <i className="bi bi-pencil"></i>
-            <i
-              className="bi bi-trash"
-              onClick={() => handleDelete(task.id)}
-            ></i>
-          </div>
-        </div>
-      ))
-    ) : (
-      <p>Não há tarefas cadastradas</p>
-    )}
-  </>
+        ))
+      ) : (
+        <p>Não há tarefas cadastradas</p>
+      )}
+    </>
   );
 };
 
